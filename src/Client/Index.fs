dd1b826eaa74103680c61a1e9e20766b2203902f
dedale <ded@laposte.net>
2021-01-01 18:27:19 +0100

    Render queue

--- src/Client/Index.fs
+++ src/Client/Index.fs
@@ -6,14 +6,16 @@ open Thoth.Fetch
 open Shared
 
 type Model =
-    { Hello: string }
+    { Hello: string
+      Status: BuildStatus }
 
 type Msg =
     | GotHello of string
 
 let init() =
     let model : Model =
-        { Hello = "" }
+        { Hello = ""
+          Status = Requests [ BuildRequest.create() ] }
     let getHello() = Fetch.get<unit, string> Route.hello
     let cmd = Cmd.OfPromise.perform getHello () GotHello
     model, cmd
@@ -24,6 +26,48 @@ let update msg model =
         { model with Hello = hello }, Cmd.none
 
 open Feliz
+
+let renderRequest request =
+    Html.tr [
+        prop.style [
+            style.border (1, borderStyle.solid, "lightgray")
+        ]
+        prop.children [
+            Html.td request.User
+            Html.td [
+                Html.a [
+                    prop.style [ style.fontFamily "monospace" ]
+                    prop.href (sprintf "http://github/owner/repo/commit/%A" request.Sha1)
+                    prop.text (request.Sha1.ToString().Substring(0, 7))
+                ]
+            ]
+            Html.td [
+                prop.style [ style.fontFamily "monospace" ]
+                prop.text (request.Platform.ToString())
+            ]
+            Html.td [
+                prop.style [ style.fontFamily "monospace" ]
+                prop.text (request.Configuration.ToString())
+            ]
+        ]
+    ]
+
+let renderQueue model =
+    Html.table [
+        prop.style [
+            style.marginLeft length.auto
+            style.marginRight length.auto
+            style.borderCollapse.collapse
+        ]
+        prop.children [
+            Html.tbody [
+                match model.Status with
+                | Requests requests ->
+                    for r in requests do
+                        yield renderRequest r
+            ]
+        ]
+    ]
 
 let view model dispatch =
     Html.div [ 
@@ -37,6 +81,7 @@ let view model dispatch =
                 Html.img [ prop.src "favicon.png" ]
                 Html.h1 "safe_minimal"
                 Html.h2 model.Hello
+                renderQueue model
             ]
         ]
     ]
