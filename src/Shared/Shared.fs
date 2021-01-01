2883966ee55ac87dd93458f1391f5b0b164cc23f
dedale <ded@laposte.net>
2021-01-01 18:17:58 +0100

    Add BuildRequest & BuildStatus

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -1,4 +1,51 @@
 namespace Shared
 
+open System
+
 module Route =
     let hello = "/api/hello"
+
+type Platform =
+    Win32 | X64
+    override x.ToString() =
+        match x with
+        | Win32 -> "Win32"
+        | X64 -> "x64"
+
+type Configuration = Debug | Release
+
+[<AutoOpen>]
+module Sha1 =
+    type Sha1 =
+        private { Value : string }
+        override x.ToString() = x.Value
+
+    let create str =
+        let m = System.Text.RegularExpressions.Regex.Match(str, "^[a-z\d]{40}$")
+        if m.Success then Ok { Sha1.Value = str }
+        else Error "Sha1 should be 40 alpha num"
+
+    let random = Random()
+
+    let createRandom () =
+        let value =
+            seq { 1..5 }
+            |> Seq.map (fun _ -> random.Next().ToString("x8"))
+            |> String.Concat
+        { Sha1.Value = value }
+
+type BuildRequest =
+    { Id : Guid
+      User : string
+      Sha1 : Sha1
+      Platform : Platform
+      Configuration : Configuration }
+    static member create () =
+        { Id = Guid.NewGuid()
+          User = "ded"
+          Sha1 = Sha1.createRandom()
+          Platform = X64
+          Configuration = Release }
+
+type BuildStatus =
+    | Requests of BuildRequest list
